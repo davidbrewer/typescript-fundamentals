565e24700c57f6a0f78d597ec81c690c946ed080
David Brewer <david.brewer@instrument.com>
2019-03-07 14:36:04 -0800

    Got dealer exercise tests passing.

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,76 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+};
 
-// }
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+};
+
+type Card = [Suit, CardNumber];
+type Deck = Card[];
+
+export class Dealer {
+  deck: Deck;
+
+  constructor() {
+    this.deck = Dealer.getNewDeck();
+    shuffleArray(this.deck);
+  }
+
+  static getNewDeck(): Deck {
+    let deck: Deck = []
+    let suit: number, num: number;
+    for (suit = 0; suit <= 3; suit++) {
+      for (num = 0; num <= 12; num++) {
+        deck.push([suit, num]);
+      }
+    }
+    return deck;
+  }
+
+  // Deals size number of cards
+  dealHand(size: number): Card[] {
+    if (size < 1) {
+      throw "Oops, can't deal negative cards";
+    }
+
+    let hand: Card[] = [];
+    while (hand.length < size) {
+      let card = this.deck.shift();
+      if (!card) {
+        throw "Oops, ran out of cards";
+      }
+      hand.push(card);
+    }
+    return hand;
+  }
+
+  // returns number of cards left in deck
+  getLength(): number {
+    return this.deck.length;
+  }
+
+  // Given a card type, convert it to a string.
+  // Example: readCard(card) => "Seven of Spades"
+  readCard(card: Card): string {
+    return `${CardNumber[card[1]]} of ${Suit[card[0]]}`
+  }
+}
