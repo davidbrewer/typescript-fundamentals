c811819341fa49868acb7e05617d24abbda69515
David Brewer <david.brewer@instrument.com>
2019-03-21 15:04:59 -0700

    Implemented cashier exercise.

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,34 @@
-// export function cashier() {
-//   return {};
-// }
+type CartItem = {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+type CartAPI = {
+  length: number;
+  total: number;
+  add: (name: string, price: number, qty?: number) => CartAPI;
+  addItem: (item: CartItem) => CartAPI;
+}
+
+export function cashier(): CartAPI {
+  let length:number = 0;
+  let total:number = 0;
+
+  return {
+    get length() {
+      return length;
+    },
+    get total() {
+      return total;
+    },
+    add: function(name, price, qty=1): CartAPI {
+      return this.addItem({name, price, qty});
+    },
+    addItem: function(item): CartAPI {
+      length += item.qty;
+      total += (item.qty * item.price);
+      return this;
+    }
+  };
+}
